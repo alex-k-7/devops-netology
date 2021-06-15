6308ca6a93ff5656fab15bb7490ea9e1ce36a94b
Aleksandr Kosogorskii <k.alex.spb@gmail.com>
2021-06-15 12:32:47 +0300

    homework revision

--- homeworks/4_3/4_3_2.py
+++ homeworks/4_3/4_3_2.py
@@ -2,25 +2,23 @@
 
 import socket, json, yaml
 
-f = open('srv_ip.txt', 'r')
-srv_list = [l.strip() for l in f]
+fr = open('srv_ip.txt', 'r')
+srv_list = [l.strip() for l in fr]
+fr.close()
 srv_dict = dict(i.split(' - ') for i in srv_list)
-f = open('srv_ip.txt', 'w')
+fw = open('srv_ip.txt', 'w')
 
 for key, value in srv_dict.items():
     ip = socket.gethostbyname(key)
     if ip != value:
         print('ERROR ', key, ' IP mismatch: ', value, ' ', ip)
-        f.write(key + ' - ' + ip + '\n')
+        fw.write(key + ' - ' + ip + '\n')
+        srv_dict[key] = ip
     else:
         print(key, ' - ', value)
-        f.write(key + ' - ' + value + '\n')
+        fw.write(key + ' - ' + value + '\n')
 
-f.close()
-
-f = open('srv_ip.txt', 'r')
-srv_list = [l.strip() for l in f]
-srv_dict = dict(i.split(' - ') for i in srv_list)
+fw.close()
 
 with open('srv_ip.json', 'w') as tmp:
     tmp.write(json.dumps(srv_dict, indent=2))
