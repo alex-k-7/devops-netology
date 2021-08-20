4b087b225bf37ac19d1f656ede34bb6fbad05e4e
Aleksandr Kosogorskii <k.alex.spb@gmail.com>
2021-08-20 16:28:36 +0300

    hw 7.2 v1

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "first_ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Test"
+  }
+  private_ip = "172.31.31.31"
+  private_dns = "first_ec2"
+  associate_public_ip_address = true
+}
