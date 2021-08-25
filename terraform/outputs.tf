1de3dfd8067191001c02a926a2406a7f93befd52
Aleksandr Kosogorskii <k.alex.spb@gmail.com>
2021-08-25 14:34:13 +0300

    hw 7.3 v1

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,11 +9,3 @@ output "user_id" {
 output "region" {
   value = data.aws_region.current.name
 }
-
-output "private_ip" {
-  value = aws_instance.first_ec2.private_ip
-}
-
-output "subnet_id" {
-  value = aws_instance.first_ec2.subnet_id
-}
