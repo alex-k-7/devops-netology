9bb8d3821684e4b7df6fd268ab1cd263a31f24dd
Aleksandr Kosogorskii <k.alex.spb@gmail.com>
2021-08-25 14:42:53 +0300

    rename files

---
+++ homeworks/7-terraform/7.2-syntax/outputs.tf
@@ -0,0 +1,19 @@
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "user_id" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+output "region" {
+  value = data.aws_region.current.name
+}
+
+output "private_ip" {
+  value = aws_instance.first_ec2.private_ip
+}
+
+output "subnet_id" {
+  value = aws_instance.first_ec2.subnet_id
+}
